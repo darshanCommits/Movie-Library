b5af9364f2923b72db50e49d8e5694e743a1fd57
Darshan Kumawat <kumawatdarshan.1304@gmail.com>
2023-04-14 15:11:46 +0530

    misc feature

--- app.js
+++ app.js
@@ -5,12 +5,23 @@ const addMovie = document.getElementById("create__new__card");
 const addNewMovie = document.getElementById("new__movie__form");
 let library = [];
 
+function caseFix(str) {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map((x) => {
+      if (typeof x === "string") return x.charAt(0).toUpperCase() + x.slice(1);
+      return x;
+    })
+    .join(" ");
+}
+
 function Movie(title, director, release, genre, plot) {
-  this.title = title;
-  this.director = director;
-  this.release = release;
-  this.genre = genre;
-  this.plot = plot;
+  this.title = caseFix(title);
+  this.director = caseFix(director);
+  this.release = caseFix(release);
+  this.genre = caseFix(genre);
+  this.plot = caseFix(plot);
 }
 
 document.getElementById("submitForm").addEventListener("click", (e) => {
@@ -38,13 +49,13 @@ function createCard(movie) {
   const card = createElem("div", "", "card");
 
   const title = createElem("h2", movie.title);
-  const director = createElem("h2", movie.director);
+  const director = createElem("h2", `By ${movie.director}`);
   const plot = createElem("p", movie.plot);
   const hr = createElem("hr");
   const releaseDate = createElem("h3", movie.release);
   const genre = createElem("h3", movie.genre);
 
-  card.append(title, director, hr, releaseDate, genre, plot);
+  card.append(title, director, plot, hr, releaseDate, genre);
 
   movieSection.appendChild(card);
 }
