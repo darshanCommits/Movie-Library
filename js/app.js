3617bc96fd3ab65197b9f88dc3702e65dbe7c6ef
Darshan Kumawat <kumawatdarshan.1304@gmail.com>
2023-04-16 19:13:06 +0530

    major refactor

--- js/app.js
+++ js/app.js
@@ -10,140 +10,154 @@ let isWatched = document.getElementsByClassName("status");
 let library = [];
 
 function Movie(title, director, release, genre, plot) {
-  this.title = caseFix(title);
-  this.director = caseFix(director);
-  this.release = caseFix(release);
-  this.genre = caseFix(genre);
-  this.plot = caseFix(plot);
-  this.watched = false;
+    this.title = caseFix(title);
+    this.director = caseFix(director);
+    this.release = caseFix(release);
+    this.genre = caseFix(genre);
+    this.plot = caseFix(plot);
+    this.watched = false;
+}
+
+function getMovieObjectWithCardIndex(card) {
+    let index = card.dataset.index;
+
+    return library[index];
 }
 
 function caseFix(str) {
-  return str
-    .toLowerCase()
-    .split(" ")
-    .map((x) => {
-      if (typeof x === "string") return x.charAt(0).toUpperCase() + x.slice(1);
-      return x;
-    })
-    .join(" ");
+    return str
+        .toLowerCase()
+        .split(" ")
+        .map((x) => {
+            if (typeof x === "string")
+                return x.charAt(0).toUpperCase() + x.slice(1);
+            return x;
+        })
+        .join(" ");
 }
 
 function fillDetails() {
-  let title = document.getElementById("title").value;
-  let director = document.getElementById("director").value;
-  let release = document.getElementById("release").value;
-  let genre = document.getElementById("genre").value;
-  let plot = document.getElementById("plot").value;
+    let title = document.getElementById("title").value;
+    let director = document.getElementById("director").value;
+    let release = document.getElementById("release").value;
+    let genre = document.getElementById("genre").value;
+    let plot = document.getElementById("plot").value;
 
-  return new Movie(title, director, release, genre, plot);
+    return new Movie(title, director, release, genre, plot);
 }
 
 function createElem(tag, text, className) {
-  const element = document.createElement(tag);
-  if (text) element.textContent = text;
-  if (className) element.classList.add(...className);
-  return element;
+    const element = document.createElement(tag);
+    if (text) element.textContent = text;
+    if (className) element.classList.add(...className);
+    return element;
 }
 
 function addToLibrary(movie) {
-  library.push(movie);
-  createCard(movie);
+    library.push(movie);
+    createCard(movie, library.length - 1);
 }
 
 function createNew(e) {
-  if (e.target.id === "submitForm") {
-    addToLibrary(fillDetails());
-  }
-  main.classList.toggle("go__up");
-  addMovie.classList.toggle("go__down");
-  document.getElementById("body").classList.toggle("height100");
+    if (e.target.id === "submitForm") {
+        addToLibrary(fillDetails());
+    }
+    main.classList.toggle("go__up");
+    addMovie.classList.toggle("go__down");
+    document.getElementById("body").classList.toggle("height100");
 }
 
-function createCard(movie) {
-  const card = createElem("div", "", ["card"]);
-  {
-    const readWrapper = createElem("div", "", ["cross-icon", "icon", "status"]);
-    const read = createElem("i", "", ["fa-solid", "fa-x", "fa-xl"]);
-    readWrapper.append(read);
+function createCard(movie, index) {
+    const card = createElem("div", "", ["card"]);
+    {
+        const readWrapper = createElem("div", "", [
+            "cross-icon",
+            "icon",
+            "status",
+        ]);
+        const read = createElem("i", "", ["fa-solid", "fa-x", "fa-xl"]);
+        readWrapper.append(read);
 
-    const trashWrapper = createElem("div", "", ["trash-icon", "icon"]);
-    const trash = createElem("i", "", ["fa-solid", "fa-trash-can", "fa-xl"]);
-    trashWrapper.append(trash);
+        const trashWrapper = createElem("div", "", ["trash-icon", "icon"]);
+        const trash = createElem("i", "", [
+            "fa-solid",
+            "fa-trash-can",
+            "fa-xl",
+        ]);
+        trashWrapper.append(trash);
 
-    const title = createElem("h2", movie.title, ["title"]);
-    const director = createElem("h2", `By ${movie.director}`, ["director"]);
-    const plot = createElem("p", movie.plot, ["plot"]);
-    const hr = createElem("hr");
-    const releaseDate = createElem("h3", movie.release, ["release"]);
-    const genre = createElem("h3", movie.genre, ["genre"]);
+        const title = createElem("h2", movie.title, ["title"]);
+        const director = createElem("h2", `By ${movie.director}`, ["director"]);
+        const plot = createElem("p", movie.plot, ["plot"]);
+        const hr = createElem("hr");
+        const releaseDate = createElem("h3", movie.release, ["release"]);
+        const genre = createElem("h3", movie.genre, ["genre"]);
 
-    card.append(
-      readWrapper,
-      trashWrapper,
-      title,
-      director,
-      plot,
-      hr,
-      releaseDate,
-      genre
-    );
-  }
+        card.append(
+            readWrapper,
+            trashWrapper,
+            title,
+            director,
+            plot,
+            hr,
+            releaseDate,
+            genre
+        );
+    }
+    card.dataset.index = index;
 
-  movieSection.appendChild(card);
+    movieSection.appendChild(card);
 }
 
 function updateInfo(e) {
-  const info = document.getElementById("info");
+    const info = document.getElementById("info");
 
-  const textOfCard = (i) => {
-    const text = [...e.target.closest("div.card").children][i].textContent;
-    return text;
-  };
+    const card = e.target.closest("div.card");
+    const movie = getMovieObjectWithCardIndex(card);
+    const textOfInfo = (i, text) => {
+        [...info.children][i].children[1].textContent = text;
+    };
 
-  const textOfInfo = (i, text) => {
-    [...info.children][i].children[1].textContent = text;
-  };
-
-  for (let i = 0; i <= 4; i++) {
-    let j = i + 2;
-    if (j >= 5) j++;
-    const cardText = textOfCard(j);
-    textOfInfo(i, cardText);
-  }
+    for (let i = 0; i <= 4; i++) {
+        const cardText = Object.values(movie)[i];
+        textOfInfo(i, cardText);
+    }
 }
 
 function deleteFn(e) {
-  let card = e.target.closest("div.card");
-  card.classList.add("fade-out");
-  setTimeout(() => card.remove(), 250);
+    let card = e.target.closest("div.card");
+    card.classList.add("fade-out");
+    setTimeout(() => card.remove(), 250);
 }
 
 function toggleRead(card) {
-  let parent = card.children[0];
-  let svg = [...parent.children][0];
+    let parent = card.children[0];
+    let svg = [...parent.children][0];
 
-  parent.classList.toggle(["check-icon"]);
-  parent.classList.toggle(["cross-icon"]);
+    parent.classList.toggle(["check-icon"]);
+    parent.classList.toggle(["cross-icon"]);
 
-  svg.classList.toggle(["fa-x"]);
-  svg.classList.toggle(["fa-check"]);
+    svg.classList.toggle(["fa-x"]);
+    svg.classList.toggle(["fa-check"]);
 }
 
 document.getElementById("submitForm").addEventListener("click", (e) => {
-  e.preventDefault();
+    e.preventDefault();
 });
 
 [...blob].forEach((x) => x.addEventListener("click", createNew));
 
 document.body.addEventListener("click", (e) => {
-  let card = e.target.closest("div.card");
+    let card = e.target.closest("div.card");
 
-  if (e.target.closest(".fa-trash-can")) {
-    deleteFn(e);
-  } else if (e.target.closest(".status")) {
-    toggleRead(card);
-  } else if (e.target.closest(".card")) {
-    updateInfo(e);
-  }
+    // let bruh = doesSomething(card);
+    // console.table(card.dataset.index, bruh);
+
+    if (e.target.closest(".fa-trash-can")) {
+        deleteFn(e);
+    } else if (e.target.closest(".status")) {
+        toggleRead(card);
+    } else if (e.target.closest(".card")) {
+        updateInfo(e);
+    }
 });
